3d7f38171fc8254d7ae893920b36c2dfe274ee7b
Mariano <petotronco@gmail.com>
2020-03-31 22:28:19 -0300

    mensajes de error

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // Variables (disponibles en todo el documento)
 const presupuestoUsuario = prompt('Cual es tu presupuesto semanal?');
+const formulario = document.getElementById('agregar-gasto');
 let cantidadPresupuesto;
 
 
@@ -29,6 +30,26 @@ class Interfaz{
         presupuestoSpan.innerHTML = `${cantidad}`;
         restanteSpan.innerHTML = `${cantidad}`;
     }
+
+    imprimirMensaje(mensaje, tipo){
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert');
+        if(tipo === 'error'){
+            divMensaje.classList.add('alert-danger');
+        } else {
+            ivMensaje.classList.add('alert-success');
+        }
+        divMensaje.appendChild(document.createTextNode(mensaje));
+
+        // insertar en el DOM
+        document.querySelector('.primario').insertBefore(divMensaje, formulario);
+
+        // quitar alert despues de 3 sg
+        setTimeout(function(){
+            document.querySelector('.primario .alert').remove();
+            formulario.reset();
+        }, 3000)
+    }
 }
 
 
@@ -47,4 +68,26 @@ document.addEventListener('DOMContentLoaded', function(){
         const ui = new Interfaz();
         ui.insertarPresupuesto(cantidadPresupuesto.presupuesto);
     }
-})
+});
+
+formulario.addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    // leer del formulario de gastos
+    const nombreGasto = document.querySelector('#gasto').value;
+    const cantidadGasto = document.querySelector('#cantidad').value;
+
+    // instanciar interfaz
+    const ui = new Interfaz();
+
+    // comprobar que los campos no esten vacios
+    if(nombreGasto === '' || cantidadGasto === ''){
+        // console.log('hubo un error!');
+
+        // 2 parametros: mensaje y tipo
+        ui.imprimirMensaje('Hubo un error', 'error');
+    }else{
+        console.log('gasto se agrego');
+    }
+
+});
